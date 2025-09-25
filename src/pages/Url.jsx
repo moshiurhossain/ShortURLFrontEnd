d5699404c15f54f9729fa8f4a28d5c9be12b2fe7
unknown <pantho883@gmail.com>
2025-09-26 03:51:20 +0600

    url ui done

--- src/pages/Url.jsx
+++ src/pages/Url.jsx
@@ -4,8 +4,13 @@ const Url = () => {
   return (
     <div className='bg-[#128296] h-screen dark:bg-[#2b2929] py-5 flex justify-center items-center'>
         <div className='w-[500px] bg-[#b2bbce] mx-auto p-4 rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'>
-            <h2 className='text-xl font-meduim text-gray-500 font-roboto'>Short Url</h2>
-            <input type="text" className=''/>
+            <h2 className=' text-[14px] lg:text-xl font-semibold text-gray-500 font-roboto'>Short Url</h2>
+            <form className='w-full border py-2 px-3 rounded-[5px] mt-8 bg-gray-100 flex justify-between'>
+            <input type="text" className='border-none outline-none lg:text-xl text-[13px]' placeholder='Enter Url'/>
+            <button className='font-roboto font-medium text-[14px] lg:text-[16px] py-2 px-5 bg-gray-300 rounded-[3px]'>Submit</button>
+            </form>
+            <h2 className='font-normal font-roboto text-[12px] lg:text-[14px] text-gray-300 my-8'>Long url</h2>
+             <a target='_blank' className='font-normal font-roboto text-[14px] lg:text-[16px] text-gray-500 ' href="#">Shor url</a>
         </div>
     </div>
   )
