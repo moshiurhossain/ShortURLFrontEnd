001127c60e85e976b093dc7d24b57335f0c9abdd
unknown <pantho883@gmail.com>
2025-09-27 02:15:17 +0600

    long url displayed

--- src/pages/Url.jsx
+++ src/pages/Url.jsx
@@ -6,30 +6,61 @@ import { FaCheckCircle } from "react-icons/fa";
 const Url = () => {
 // copy , setCopy useState starts
 const [copy,setCopy]=useState(false)
+
+// short url useState starts
+const [longUrl,setLongUrl]=useState('')
+
+// error useState starts
+const [myerror,setMyError]=useState('')
+
+// displaylongUrl useState starts
+const [display,setDisplay]=useState('')
+
 // handleCopy function starts
 const handleCopy = ()=>{
+
   //  copy set to true
    setCopy(!copy)
+
   //  copy set to false after 2.5seconds
    setTimeout(()=>setCopy(copy),2500)
+
   //  copy text
   const copiedText = document.querySelector('.copiedText')
+
   //  Copy the text inside the text field
- navigator.clipboard.writeText(copiedText.textContent);
+  navigator.clipboard.writeText(copiedText.textContent);
 
 
   console.log('text coppied')
+}
+// handleShort function Starts
+const handleShort =()=>{
+ if(!longUrl) return setMyError('Alert input is empty')
+  setMyError('')
+ 
+ 
+ setDisplay(longUrl)
+ setLongUrl('')
 }
 
   return (
     <div className='bg-[#128296] h-screen dark:bg-[#2b2929] py-5 flex justify-center items-center'>
         <div className='w-[500px] bg-[#b2bbce] mx-auto p-4 rounded-lg shadow-[0px_0px_0px_1px_rgba(0,0,0,0.06),0px_1px_1px_-0.5px_rgba(0,0,0,0.06),0px_3px_3px_-1.5px_rgba(0,0,0,0.06),_0px_6px_6px_-3px_rgba(0,0,0,0.06),0px_12px_12px_-6px_rgba(0,0,0,0.06),0px_24px_24px_-12px_rgba(0,0,0,0.06)]'>
             <h2 className=' text-[14px] lg:text-xl font-semibold text-gray-500 font-roboto'>Short Url</h2>
+             <p className='text-[12px] text-red-400 font-roboto mt-3'>{myerror}</p>
             <form className='w-full border py-2 px-3 rounded-[5px] mt-8 bg-gray-100 flex justify-between'>
-            <input type="text" className='border-none outline-none lg:text-xl text-[13px]' placeholder='Enter Url'/>
-            <button className='font-roboto font-medium text-[14px] lg:text-[16px] py-2 px-5 bg-gray-300 rounded-[3px]'>Submit</button>
+            <input 
+            value={longUrl}
+            onChange={(e)=>setLongUrl(e.target.value)}
+            type="text" className='border-none outline-none lg:text-xl text-[13px]' placeholder='Enter Url'/>
+            <button
+            onClick={handleShort}
+            className='active:scale-[1.1] cursor-pointer font-roboto font-medium text-[14px] lg:text-[16px] py-2 px-5 bg-gray-300 rounded-[3px]'>Submit</button>
             </form>
-            <h2 className='font-normal font-roboto text-[12px] lg:text-[14px] text-gray-300 my-8'>Long url</h2>
+            
+            <h2 className='font-normal font-roboto text-[12px] lg:text-[14px] text-gray-300 my-8'>Long url <span className='ml-3 overflow-hidden '>{display}</span></h2>
+           
             <div className='flex justify-between'>
              <a target='_blank' className='font-normal font-roboto text-[14px] lg:text-[16px] text-gray-500 ' href="#">Short url
               <span className='ml-4 font-medium copiedText'>Url link here</span>
